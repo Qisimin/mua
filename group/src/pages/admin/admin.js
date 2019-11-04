846c961e227ef57a6a6535d655509638d9d4672e
Qisimin <1726556836@qq.com>
2019-11-04 14:02:22 +0800

    npm install

---
+++ group/src/pages/admin/admin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Admin extends React.Component{
+    render(){
+        return(
+            <div>
+                这里是admin
+            </div>
+        )
+    }
+}
+export default Admin
