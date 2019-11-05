33e81ab36a2f951852f65316d6659589ab999292
Qisimin <1726556836@qq.com>
2019-11-05 13:58:32 +0800

    router

--- group/src/index.js
+++ group/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import App from './App';
-import App from './pages/admin/admin'
+// import App from './pages/admin/admin'
+import App from './router'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- group/src/pages/admin/admin.js
+++ group/src/pages/admin/admin.js
@@ -1,10 +1,38 @@
 import React from 'react'
-
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import CustomSlider from '../../components/slider/slider'
+import Style from './admin.module.less'
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 class Admin extends React.Component{
     render(){
         return(
             <div>
-                这里是admin页面。
+            <Layout>
+                <Header className={Style.header}>
+                
+                </Header>
+                <Layout>
+                    <Sider width={255} style={{ background: '#fff' }} className={Style.up}>
+                        <CustomSlider></CustomSlider>
+                    </Sider>
+                    <Layout style={{ padding: '5px' }}>
+                        
+                        <Content 
+                        style={{
+                            // background: '#fff',
+                            padding: 24,
+                            margin: 0,
+                            minHeight: 450,
+                            // maxHeight:550
+                        }}
+                        className={Style.bg}
+                        >
+                            Content......................
+                        </Content>
+                    </Layout>
+                </Layout>
+            </Layout>
             </div>
         )
     }
